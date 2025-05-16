985f9a289881e8e3c348a7a9c363f90958954d27
Vignesh Saravanakumar <vignesh.saravanakumar.vs@gmail.com>
2025-05-16 17:09:07 -0400

    Import Templates

--- Homeworks/Homework 3/Strings.java
+++ Homeworks/Homework 3/Strings.java
@@ -9,6 +9,9 @@ interface ILoString {
   // returns true if the list is sorted
   boolean isSorted();
 
+  // returns true if the list is sorted
+  boolean isSortedHelper(String prev);
+
   // returns a sublist of this list from start to end
   ILoString sublist(int start, int end);
 
@@ -29,7 +32,7 @@ interface ILoString {
   // to check if the list is a palindrome
   boolean isPalindromeList();
 
-  // reverse
+  // reversethe list
   ILoString reverse();
 
   // reverse helper with accumulator
@@ -47,16 +50,24 @@ class MtLoString implements ILoString {
   /*
    * TEMPLATE
    * FIELDS:
-   * ... this ... -- ILoString
+   * none
    * 
-   * METHODS
+   * METHODS:
    * ... this.combine() ... -- String
    * ... this.isSorted() ... -- boolean
+   * ... this.isSortedHelper(String) ... -- boolean
    * ... this.sublist(int, int) ... -- ILoString
    * ... this.interleave(ILoString) ... -- ILoString
+   * ... this.merge(ILoString) ... -- ILoString
+   * ... this.mergeHelper(ConsLoString) ... -- ILoString
+   * ... this.isDoubledList() ... -- boolean
+   * ... this.isDoubledListHelper(String) ... -- boolean
+   * ... this.isPalindromeList() ... -- boolean
+   * ... this.reverse() ... -- ILoString
+   * ... this.reverseAcc(ILoString) ... -- ILoString
    * 
-   * METHODS FOR FIELDS
-   * 
+   * METHODS FOR FIELDS:
+   * none
    */
 
   // combine all Strings in this list into one
@@ -69,6 +80,11 @@ class MtLoString implements ILoString {
     return true;
   }
 
+  // returns true if the list is sorted
+  public boolean isSortedHelper(String prev) {
+    return true;
+  }
+
   // returns a sublist of this list from start to end
   public ILoString sublist(int start, int end) {
     return this;
@@ -76,11 +92,55 @@ class MtLoString implements ILoString {
 
   // returns a new list that is the interleaving of this list and the other list
   public ILoString interleave(ILoString other) {
+    /*
+     * METHOD TEMPLATE: everything in the class template for MtLoString, plus
+     *
+     * PARAMETERS:
+     * ... other ... -- ILoString
+     *
+     * METHODS ON/OF/FOR PARAMETERS:
+     * ... other.combine() ... -- String
+     * ... other.isSorted() ... -- boolean
+     * ... other.isSortedHelp(String) ... -- boolean
+     * ... other.sublist(int, int) ... -- ILoString
+     * ... other.interleave(ILoString) ... -- ILoString
+     * ... other.merge(ILoString) ... -- ILoString
+     * ... other.mergeHelp(String, ILoString) ... -- ILoString
+     * ... other.isDoubledList() ... -- boolean
+     * ... other.isDoubledListHelp(String) ... -- boolean
+     * ... other.isPalindromeList() ... -- boolean
+     * ... other.length() ... -- int
+     * ... other.reverse() ... -- ILoString
+     * ... other.reverseHelp() ... -- ILoString
+     * 
+     */
     return other;
   }
 
   // merging with empty list returns the other list
   public ILoString merge(ILoString other) {
+    /*
+     * METHOD TEMPLATE: everything in the class template for MtLoString, plus
+     *
+     * PARAMETERS:
+     * ... other ... -- ILoString
+     *
+     * METHODS ON/OF/FOR PARAMETERS:
+     * ... other.combine() ... -- String
+     * ... other.isSorted() ... -- boolean
+     * ... other.isSortedHelp(String) ... -- boolean
+     * ... other.sublist(int, int) ... -- ILoString
+     * ... other.interleave(ILoString) ... -- ILoString
+     * ... other.merge(ILoString) ... -- ILoString
+     * ... other.mergeHelp(String, ILoString) ... -- ILoString
+     * ... other.isDoubledList() ... -- boolean
+     * ... other.isDoubledListHelp(String) ... -- boolean
+     * ... other.isPalindromeList() ... -- boolean
+     * ... other.length() ... -- int
+     * ... other.reverse() ... -- ILoString
+     * ... other.reverseHelp() ... -- ILoString
+     * 
+     */
     return other;
   }
 
@@ -110,6 +170,28 @@ class MtLoString implements ILoString {
 
   // reverse helper with accumulator
   public ILoString reverseAcc(ILoString acc) {
+    /*
+     * METHOD TEMPLATE: everything in the class template for MtLoString, plus
+     *
+     * PARAMETERS:
+     * ... other ... -- ILoString
+     *
+     * METHODS ON/OF/FOR PARAMETERS:
+     * ... other.combine() ... -- String
+     * ... other.isSorted() ... -- boolean
+     * ... other.isSortedHelp(String) ... -- boolean
+     * ... other.sublist(int, int) ... -- ILoString
+     * ... other.interleave(ILoString) ... -- ILoString
+     * ... other.merge(ILoString) ... -- ILoString
+     * ... other.mergeHelp(String, ILoString) ... -- ILoString
+     * ... other.isDoubledList() ... -- boolean
+     * ... other.isDoubledListHelp(String) ... -- boolean
+     * ... other.isPalindromeList() ... -- boolean
+     * ... other.length() ... -- int
+     * ... other.reverse() ... -- ILoString
+     * ... other.reverseHelp() ... -- ILoString
+     * 
+     */
     return acc;
   }
 
@@ -132,17 +214,29 @@ class ConsLoString implements ILoString {
    * ... this.first ... -- String
    * ... this.rest ... -- ILoString
    * 
-   * METHODS
+   * METHODS:
    * ... this.combine() ... -- String
    * ... this.isSorted() ... -- boolean
+   * ... this.isSortedHelper(String) ... -- boolean
    * ... this.sublist(int, int) ... -- ILoString
    * ... this.interleave(ILoString) ... -- ILoString
+   * ... this.merge(ILoString) ... -- ILoString
+   * ... this.mergeHelper(ConsLoString) ... -- ILoString
+   * ... this.isDoubledList() ... -- boolean
+   * ... this.isDoubledListHelper(String) ... -- boolean
+   * ... this.isPalindromeList() ... -- boolean
+   * ... this.reverse() ... -- ILoString
+   * ... this.reverseAcc(ILoString) ... -- ILoString
    * 
-   * METHODS FOR FIELDS
-   * ... this.first.concat(String) ... -- String
-   * ... this.first.compareTo(String) ... -- int
+   * METHODS FOR FIELDS:
    * ... this.rest.combine() ... -- String
-   * 
+   * ... this.rest.isSortedHelper(String) ... -- boolean
+   * ... this.rest.sublist(int, int) ... -- ILoString
+   * ... this.rest.interleave(ILoString) ... -- ILoString
+   * ... this.rest.merge(ILoString) ... -- ILoString
+   * ... this.rest.mergeHelper(ConsLoString) ... -- ILoString
+   * ... this.rest.isDoubledListHelper(String) ... -- boolean
+   * ... this.rest.reverseAcc(ILoString) ... -- ILoString
    */
 
   // combine all Strings in this list into one
@@ -152,15 +246,20 @@ class ConsLoString implements ILoString {
 
   // returns true if the list is sorted
   public boolean isSorted() {
-    return this.rest.isSorted() && this.first.compareTo(this.rest.combine()) <= 0;
+    return this.rest.isSortedHelper(this.first);
+  }
+
+  // returns true if the list is sorted
+  public boolean isSortedHelper(String prev) {
+    return this.first.compareToIgnoreCase(prev) >= 0 && this.rest.isSortedHelper(this.first);
   }
 
   // returns a sublist of this list from start to end
   public ILoString sublist(int start, int end) {
-    if (start == 0 && end == -1) {
+    if (start <= 0 && end == -1) {
       return new MtLoString();
     }
-    if (start == 0) {
+    if (start <= 0) {
       return new ConsLoString(this.first, this.rest.sublist(start, end - 1));
     }
     return this.rest.sublist(start - 1, end - 1);
@@ -169,11 +268,55 @@ class ConsLoString implements ILoString {
 
   // returns a new list that is the interleaving of this list and the other list
   public ILoString interleave(ILoString other) {
+    /*
+     * METHOD TEMPLATE: everything in the class template for MtLoString, plus
+     *
+     * PARAMETERS:
+     * ... other ... -- ILoString
+     *
+     * METHODS ON/OF/FOR PARAMETERS:
+     * ... other.combine() ... -- String
+     * ... other.isSorted() ... -- boolean
+     * ... other.isSortedHelp(String) ... -- boolean
+     * ... other.sublist(int, int) ... -- ILoString
+     * ... other.interleave(ILoString) ... -- ILoString
+     * ... other.merge(ILoString) ... -- ILoString
+     * ... other.mergeHelp(String, ILoString) ... -- ILoString
+     * ... other.isDoubledList() ... -- boolean
+     * ... other.isDoubledListHelp(String) ... -- boolean
+     * ... other.isPalindromeList() ... -- boolean
+     * ... other.length() ... -- int
+     * ... other.reverse() ... -- ILoString
+     * ... other.reverseHelp() ... -- ILoString
+     * 
+     */
     return new ConsLoString(this.first, other.interleave(this.rest));
   }
 
   // merges this non-empty list with another
   public ILoString merge(ILoString other) {
+    /*
+     * METHOD TEMPLATE: everything in the class template for MtLoString, plus
+     *
+     * PARAMETERS:
+     * ... other ... -- ILoString
+     *
+     * METHODS ON/OF/FOR PARAMETERS:
+     * ... other.combine() ... -- String
+     * ... other.isSorted() ... -- boolean
+     * ... other.isSortedHelp(String) ... -- boolean
+     * ... other.sublist(int, int) ... -- ILoString
+     * ... other.interleave(ILoString) ... -- ILoString
+     * ... other.merge(ILoString) ... -- ILoString
+     * ... other.mergeHelp(String, ILoString) ... -- ILoString
+     * ... other.isDoubledList() ... -- boolean
+     * ... other.isDoubledListHelp(String) ... -- boolean
+     * ... other.isPalindromeList() ... -- boolean
+     * ... other.length() ... -- int
+     * ... other.reverse() ... -- ILoString
+     * ... other.reverseHelp() ... -- ILoString
+     * 
+     */
     return other.mergeHelper(this); // double dispatch
   }
 
@@ -181,9 +324,8 @@ class ConsLoString implements ILoString {
   public ILoString mergeHelper(ConsLoString that) {
     if (this.first.toLowerCase().compareTo(that.first.toLowerCase()) <= 0) {
       return new ConsLoString(this.first, this.rest.merge(that));
-    } else {
-      return new ConsLoString(that.first, this.merge(that.rest));
     }
+    return new ConsLoString(that.first, this.merge(that.rest));
   }
 
   public boolean isDoubledList() {
@@ -208,6 +350,28 @@ class ConsLoString implements ILoString {
 
   // reverse helper with accumulator
   public ILoString reverseAcc(ILoString acc) {
+    /*
+     * METHOD TEMPLATE: everything in the class template for MtLoString, plus
+     *
+     * PARAMETERS:
+     * ... other ... -- ILoString
+     *
+     * METHODS ON/OF/FOR PARAMETERS:
+     * ... other.combine() ... -- String
+     * ... other.isSorted() ... -- boolean
+     * ... other.isSortedHelp(String) ... -- boolean
+     * ... other.sublist(int, int) ... -- ILoString
+     * ... other.interleave(ILoString) ... -- ILoString
+     * ... other.merge(ILoString) ... -- ILoString
+     * ... other.mergeHelp(String, ILoString) ... -- ILoString
+     * ... other.isDoubledList() ... -- boolean
+     * ... other.isDoubledListHelp(String) ... -- boolean
+     * ... other.isPalindromeList() ... -- boolean
+     * ... other.length() ... -- int
+     * ... other.reverse() ... -- ILoString
+     * ... other.reverseHelp() ... -- ILoString
+     * 
+     */
     return this.rest.reverseAcc(new ConsLoString(this.first, acc));
   }
 
@@ -219,49 +383,51 @@ class ExamplesStrings {
   ILoString emptyList = new MtLoString();
   ILoString mary = new ConsLoString("Mary ", new ConsLoString("had ", new ConsLoString("a ",
       new ConsLoString("little ", new ConsLoString("lamb.", new MtLoString())))));
-  ILoString bob = new ConsLoString("Can ", new ConsLoString("he ",
-      new ConsLoString("build ", new ConsLoString("it.", new MtLoString()))));
   ILoString sortedA = new ConsLoString("a", new ConsLoString("b",
       new ConsLoString("c", new ConsLoString("d", new ConsLoString("e", new MtLoString())))));
   ILoString sortedB = new ConsLoString("a", new ConsLoString("c",
       new ConsLoString("c", new ConsLoString("e", new ConsLoString("e", new MtLoString())))));
-  ILoString singleElement = new ConsLoString("hello", new MtLoString());
-  ILoString oddPalindrome = new ConsLoString("first",
-      new ConsLoString("middle",
-          new ConsLoString("first", new MtLoString())));
-  ILoString evenPalindrome = new ConsLoString("start",
-      new ConsLoString("middle",
-          new ConsLoString("middle",
-              new ConsLoString("start", new MtLoString()))));
-  ILoString notPalindrome = new ConsLoString("first",
-      new ConsLoString("second",
-          new ConsLoString("third", new MtLoString())));
-  ILoString perfectDoubled = new ConsLoString("apple",
-      new ConsLoString("apple",
-          new ConsLoString("banana",
-              new ConsLoString("banana",
-                  new ConsLoString("cherry",
-                      new ConsLoString("cherry", new MtLoString()))))));
-  ILoString notDoubled = new ConsLoString("apple",
-      new ConsLoString("orange",
-          new ConsLoString("banana",
-              new ConsLoString("grape", new MtLoString()))));
-  ILoString oddLength = new ConsLoString("apple",
-      new ConsLoString("apple",
-          new ConsLoString("banana", new MtLoString())));
-  ILoString singlePair = new ConsLoString("hello",
-      new ConsLoString("hello", new MtLoString()));
 
+  // Tests for combine method
   boolean testCombine(Tester t) {
-    return t.checkExpect(this.mary.combine(), "Mary had a little lamb.");
+    return t.checkExpect(this.mary.combine(), "Mary had a little lamb.") &&
+        t.checkExpect(this.emptyList.combine(), "") &&
+        t.checkExpect(this.sortedA.combine(), "abcde") &&
+        t.checkExpect(this.sortedB.combine(), "accee");
   }
 
+  // Tests for isSorted method
+  boolean testIsSorted(Tester t) {
+    return t.checkExpect(this.mary.isSorted(), false) &&
+        t.checkExpect(this.sortedA.isSorted(), true) &&
+        t.checkExpect(this.sortedB.isSorted(), true) &&
+        t.checkExpect(this.emptyList.isSorted(), true);
+  }
+
+  // tests for isSortedHelper method
+  boolean testIsSortedHelper(Tester t) {
+    return t.checkExpect(this.mary.isSortedHelper("Mary "), false) &&
+        t.checkExpect(this.sortedA.isSortedHelper("a"), true) &&
+        t.checkExpect(this.sortedB.isSortedHelper("a"), true) &&
+        t.checkExpect(this.emptyList.isSortedHelper(""), true);
+  }
+
+  // tests for sublist method
   boolean testILoStringSublist(Tester t) {
     return t.checkExpect(this.mary.sublist(1, 3), new ConsLoString("had ",
-        new ConsLoString("a ", new ConsLoString("little ", new MtLoString()))));
+        new ConsLoString("a ", new ConsLoString("little ", new MtLoString()))))
+        && t.checkExpect(this.mary.sublist(0, 1), new ConsLoString("Mary ",
+            new ConsLoString("had ", new MtLoString())))
+        && t.checkExpect(this.mary.sublist(2, 5), new ConsLoString("a ",
+            new ConsLoString("little ", new ConsLoString("lamb.", new MtLoString()))))
+        && t.checkExpect(this.mary.sublist(0, 5), this.mary);
   }
 
+  // tests for interleave method
   boolean testILoStringInterleave(Tester t) {
+    ILoString bob = new ConsLoString("Can ", new ConsLoString("he ",
+        new ConsLoString("build ", new ConsLoString("it.", new MtLoString()))));
+
     return t.checkExpect(this.mary.interleave(bob),
         new ConsLoString("Mary ",
             new ConsLoString("Can ", new ConsLoString("had ", new ConsLoString("he ",
@@ -269,9 +435,10 @@ class ExamplesStrings {
                     new ConsLoString("it.", new ConsLoString("lamb.", new MtLoString()))))))))));
   }
 
+  // tests for merge
   boolean testILoStringMerge(Tester t) {
     return t
-        .checkExpect(sortedA.merge(sortedB),
+        .checkExpect(this.sortedA.merge(this.sortedB),
             new ConsLoString("a",
                 new ConsLoString("a",
                     new ConsLoString("b", new ConsLoString("c", new ConsLoString("c",
@@ -279,22 +446,82 @@ class ExamplesStrings {
                             new ConsLoString("e", new ConsLoString("e", new MtLoString())))))))))));
   }
 
+  // tests for mergeHelper
+  boolean testILoStringMergeHelper(Tester t) {
+    return true;
+  }
+
   // Tests for isDoubledList method
   boolean testIsDoubledList(Tester t) {
+    ILoString perfectDoubled = new ConsLoString("apple",
+        new ConsLoString("apple",
+            new ConsLoString("banana",
+                new ConsLoString("banana",
+                    new ConsLoString("cherry",
+                        new ConsLoString("cherry", new MtLoString()))))));
+    ILoString notDoubled = new ConsLoString("apple",
+        new ConsLoString("orange",
+            new ConsLoString("banana",
+                new ConsLoString("grape", new MtLoString()))));
+    ILoString oddLength = new ConsLoString("apple",
+        new ConsLoString("apple",
+            new ConsLoString("banana", new MtLoString())));
+    ILoString singlePair = new ConsLoString("hello",
+        new ConsLoString("hello", new MtLoString()));
+
     return t.checkExpect(this.emptyList.isDoubledList(), true) && // Empty list edge case
-        t.checkExpect(this.perfectDoubled.isDoubledList(), true) &&
-        t.checkExpect(this.notDoubled.isDoubledList(), false) &&
-        t.checkExpect(this.oddLength.isDoubledList(), false) &&
-        t.checkExpect(this.singlePair.isDoubledList(), true);
+        t.checkExpect(perfectDoubled.isDoubledList(), true) &&
+        t.checkExpect(notDoubled.isDoubledList(), false) &&
+        t.checkExpect(oddLength.isDoubledList(), false) &&
+        t.checkExpect(singlePair.isDoubledList(), true);
+  }
+
+  // tests for isDoubledListHelper method
+  boolean testIsDoubledListHelper(Tester t) {
+    return true;
   }
 
   // Tests for isPalindromeList method
   boolean testIsPalindromeList(Tester t) {
+
+    ILoString singleElement = new ConsLoString("hello", new MtLoString());
+    ILoString oddPalindrome = new ConsLoString("first",
+        new ConsLoString("middle",
+            new ConsLoString("first", new MtLoString())));
+    ILoString evenPalindrome = new ConsLoString("start",
+        new ConsLoString("middle",
+            new ConsLoString("middle",
+                new ConsLoString("start", new MtLoString()))));
+    ILoString notPalindrome = new ConsLoString("first",
+        new ConsLoString("second",
+            new ConsLoString("third", new MtLoString())));
+
     return t.checkExpect(this.emptyList.isPalindromeList(), true) &&
-        t.checkExpect(this.singleElement.isPalindromeList(), true) &&
-        t.checkExpect(this.oddPalindrome.isPalindromeList(), true) &&
-        t.checkExpect(this.evenPalindrome.isPalindromeList(), true) &&
-        t.checkExpect(this.notPalindrome.isPalindromeList(), false);
+        t.checkExpect(singleElement.isPalindromeList(), true) &&
+        t.checkExpect(oddPalindrome.isPalindromeList(), true) &&
+        t.checkExpect(evenPalindrome.isPalindromeList(), true) &&
+        t.checkExpect(notPalindrome.isPalindromeList(), false);
   }
 
+  // tests for reverse method
+  boolean testReverse(Tester t) {
+    return t.checkExpect(this.mary.reverse(),
+        new ConsLoString("lamb.", new ConsLoString("little ",
+            new ConsLoString("a ", new ConsLoString("had ", new ConsLoString("Mary ", new MtLoString()))))))
+        && t.checkExpect(this.emptyList.reverse(), this.emptyList)
+        && t.checkExpect(this.sortedA.reverse(),
+            new ConsLoString("e", new ConsLoString("d", new ConsLoString("c",
+                new ConsLoString("b", new ConsLoString("a", new MtLoString()))))));
+  }
+
+  // tests for reverseAcc method
+  boolean testReverseAcc(Tester t) {
+    return t.checkExpect(this.mary.reverseAcc(new MtLoString()),
+        new ConsLoString("lamb.", new ConsLoString("little ",
+            new ConsLoString("a ", new ConsLoString("had ", new ConsLoString("Mary ", new MtLoString()))))))
+        && t.checkExpect(this.emptyList.reverseAcc(new MtLoString()), this.emptyList)
+        && t.checkExpect(this.sortedA.reverseAcc(new MtLoString()),
+            new ConsLoString("e", new ConsLoString("d", new ConsLoString("c",
+                new ConsLoString("b", new ConsLoString("a", new MtLoString()))))));
+  }
 }
