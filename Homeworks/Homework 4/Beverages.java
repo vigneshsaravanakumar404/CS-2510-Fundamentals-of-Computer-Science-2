09b0b2272c8e317285b4b5ca4d26078e559f3aa1
Vignesh Saravanakumar <vignesh.saravanakumar.vs@gmail.com>
2025-05-18 17:21:43 -0400

    Homework 4 Beverages.java

--- Homeworks/Homework 4/Beverages.java
+++ Homeworks/Homework 4/Beverages.java
@@ -1,20 +1,81 @@
 import tester.Tester;
 
+// Represents a drink you might get at a coffee shop
+interface IBeverage {
+
+  // returns if a drink is decaffeinated
+  boolean isDecaf();
+
+  // returns true if the drink contains the given ingredient
+  boolean containsIngredient(String ingredient);
+
+  // returns a string representation of the drink
+  String format();
+
+}
+
+abstract class ABeverage implements IBeverage {
+  // TODO: Template
+  // TODO: Purpose Statements
+
+  String variety;
+
+  ILoString mixins;
+
+  // the constructor
+  ABeverage(String variety, ILoString mixins) {
+    this.variety = variety;
+    this.mixins = mixins;
+  }
+
+  /*
+   * TEMPLATE
+   * FIELDS:
+   * ... this.variety ... -- String
+   * ... this.mixins ... -- ILoString
+   * METHODS:
+   * ... this.isDecaf() ... -- boolean
+   * ... this.containsIngredient(String) ... -- boolean
+   * ... this.format() ... -- String
+   * ... this.formatHelp(String, String) ... -- String
+   * METHODS FOR FIELDS:
+   * ... this.mixins.containsString() ... -- boolean
+   * ... this.mixins.format() ... -- String
+   * ... this.mixins.formatHelp() ... -- String
+   */
+
+  // return if the beverage is decaf
+  public abstract boolean isDecaf();
+
+  // return if the beverage contains the ingredient
+  public boolean containsIngredient(String ingredient) {
+    return this.mixins.containsIngredient(ingredient);
+  }
+
+  // return the string representation of the beverage
+  public abstract String format();
+
+}
+
+// Represents a beverage
 class BubbleTea extends ABeverage {
   // TODO: Template
   // TODO: Purpose Statements
 
   int size;
 
-  BubbleTea(String variety, int size, ILoString mixins) {
+  // the constructor
+  BubbleTea(String variety, ILoString mixins, int size) {
     super(variety, mixins);
     this.size = size;
   }
 
+  // return if the beverage is decaf
   public boolean isDecaf() {
     return this.variety.equals("Rooibos");
   }
 
+  // return if the beverage contains the ingredient
   public String format() {
     String mixIns = this.mixins.format();
 
@@ -34,16 +95,19 @@ class Coffee extends ABeverage {
 
   boolean isIced;
 
-  Coffee(String variety, String style, boolean isIced, ILoString mixins) {
+  // the constructor
+  Coffee(String variety, ILoString mixins, String style, boolean isIced) {
     super(variety, mixins);
     this.style = style;
     this.isIced = isIced;
   }
 
+  // return if the beverage is decaf
   public boolean isDecaf() {
     return false;
   }
 
+  // return if the beverage contains the ingredient
   public String format() {
     String mixIns = this.mixins.format();
     String iced = "Hot ";
@@ -52,13 +116,14 @@ class Coffee extends ABeverage {
       iced = "Iced ";
     }
     if (mixIns.equals("")) {
-      return iced + this.variety + " (without mixins)";
+      return iced + this.variety + " " + this.style + " (without mixins)";
     }
 
-    return iced + this.variety + " (with " + mixIns + ")";
+    return iced + this.variety + " " + this.style + " (with " + mixIns + ")";
   }
 }
 
+// Represents a beverage
 class Milkshake extends ABeverage {
   // TODO: Template
   // TODO: Purpose Statements
@@ -67,21 +132,24 @@ class Milkshake extends ABeverage {
 
   int size;
 
-  Milkshake(String flavor, String brandName, int size, ILoString toppings) {
+  // the constructor
+  Milkshake(String flavor, ILoString toppings, String brandName, int size) {
     super(flavor, toppings);
     this.brandName = brandName;
     this.size = size;
   }
 
+  // return if the beverage is decaf
   public boolean isDecaf() {
     return true;
   }
 
+  // return if the beverage contains the ingredient
   public String format() {
     String mixIns = this.mixins.format();
 
     if (mixIns.equals("")) {
-      return this.size + "oz " + this.brandName + " " + this.variety + " (without toppings)";
+      return this.size + "oz " + this.brandName + " " + this.variety + " (without mixins)";
     }
 
     return this.size + "oz " + this.brandName + " " + this.variety + " (with " + mixIns + ")";
@@ -92,8 +160,10 @@ interface ILoString {
   // TODO: Template
   // TODO: Purpose Statements
 
+  // return if the beverage contains the ingredient
   boolean containsIngredient(String ingredient);
 
+  // return the string representation of the beverage
   String format();
 }
 
@@ -101,13 +171,16 @@ class MtLoString implements ILoString {
   // TODO: Template
   // TODO: Purpose Statements
 
+  // the constructor
   MtLoString() {
   }
 
+  // return if the beverage is decaf
   public boolean containsIngredient(String ingredient) {
     return false;
   }
 
+  // return if the beverage contains the ingredient
   public String format() {
     return "";
   }
@@ -121,15 +194,18 @@ class ConsLoString implements ILoString {
   String first;
   ILoString rest;
 
+  // the constructor
   ConsLoString(String first, ILoString rest) {
     this.first = first;
     this.rest = rest;
   }
 
+  // return if the beverage is decaf
   public boolean containsIngredient(String ingredient) {
     return this.first.equals(ingredient) || this.rest.containsIngredient(ingredient);
   }
 
+  // return if the beverage contains the ingredient
   public String format() {
     if (this.rest.format().equals("")) {
       return this.first;
@@ -140,41 +216,60 @@ class ConsLoString implements ILoString {
 
 class ExamplesBeverages {
 
-  IBeverage b1 = new BubbleTea("Black tea", 16, new ConsLoString("boba", new MtLoString()));
-  IBeverage b2 = new Coffee("Arabica", "espresso", false, new ConsLoString("cream", new MtLoString()));
-  IBeverage b3 = new Milkshake("vanilla", "Ben&Jerrys", 12, new ConsLoString("whipped cream", new MtLoString()));
-  IBeverage b4 = new BubbleTea("Oolong", 20, new ConsLoString("extra sugar", new MtLoString()));
-  IBeverage b5 = new Coffee("Robusta", "americano", true, new ConsLoString("sugar", new MtLoString()));
-  IBeverage b6 = new Milkshake("mint-chip", "JPLicks", 16, new ConsLoString("sprinkles", new MtLoString()));
+  IBeverage b1 = new BubbleTea("Black tea", new ConsLoString("boba",
+      new MtLoString()), 16);
+  IBeverage b2 = new Coffee("Arabica", new ConsLoString("cream",
+      new MtLoString()), "espresso", false);
+  IBeverage b3 = new Milkshake("vanilla", new ConsLoString("whipped cream",
+      new MtLoString()), "Ben&Jerrys", 12);
+  IBeverage b4 = new BubbleTea("Oolong", new ConsLoString("extra sugar",
+      new MtLoString()), 20);
+  IBeverage b5 = new Coffee("Robusta", new ConsLoString("sugar",
+      new MtLoString()), "americano", true);
+  IBeverage b6 = new Milkshake("mint-chip", new ConsLoString("sprinkles",
+      new MtLoString()), "JPLicks", 16);
+  IBeverage b7 = new BubbleTea("Rooibos", new MtLoString(), 16);
+  IBeverage b8 = new Milkshake("chocolate", new ConsLoString("whipped cream",
+      new ConsLoString("cherry",
+          new ConsLoString("sprinkles", new MtLoString()))),
+      "HaagenDazs", 20);
 
+  // test cases for the isDecaf method
   boolean testIsDecaf(Tester t) {
     return t.checkExpect(this.b1.isDecaf(), false)
         && t.checkExpect(this.b2.isDecaf(), false)
         && t.checkExpect(this.b3.isDecaf(), true)
         && t.checkExpect(this.b4.isDecaf(), false)
         && t.checkExpect(this.b5.isDecaf(), false)
-        && t.checkExpect(this.b6.isDecaf(), true);
+        && t.checkExpect(this.b6.isDecaf(), true)
+        && t.checkExpect(this.b7.isDecaf(), true)
+        && t.checkExpect(this.b8.isDecaf(), true);
 
     // TODO: include rooibos in the test cases
   }
 
+  // test cases for the containsIngredient method
   boolean testContainsIngredient(Tester t) {
     return t.checkExpect(this.b1.containsIngredient("boba"), true)
         && t.checkExpect(this.b2.containsIngredient("cream"), true)
         && t.checkExpect(this.b3.containsIngredient("whipped cream"), true)
         && t.checkExpect(this.b4.containsIngredient("extra sugar"), true)
         && t.checkExpect(this.b5.containsIngredient("sugar"), true)
-        && t.checkExpect(this.b6.containsIngredient("sprinkles"), true);
-    // TODO: include no mixins in the test cases
+        && t.checkExpect(this.b6.containsIngredient("sprinkles"), true)
+        && t.checkExpect(this.b7.containsIngredient("boba"), false)
+        && t.checkExpect(this.b8.containsIngredient("cherry"), true);
   }
 
+  // test cases for the format method
   boolean testFormat(Tester t) {
     return t.checkExpect(this.b1.format(), "16oz Black tea (with boba)")
-        && t.checkExpect(this.b2.format(), "Hot Arabica (with cream)")
+        && t.checkExpect(this.b2.format(), "Hot Arabica espresso (with cream)")
         && t.checkExpect(this.b3.format(), "12oz Ben&Jerrys vanilla (with whipped cream)")
         && t.checkExpect(this.b4.format(), "20oz Oolong (with extra sugar)")
-        && t.checkExpect(this.b5.format(), "Iced Robusta (with sugar)")
-        && t.checkExpect(this.b6.format(), "16oz JPLicks mint-chip (with sprinkles)");
-    // TODO: include no mixins in the test cases
+        && t.checkExpect(this.b5.format(), "Iced Robusta americano (with sugar)")
+        && t.checkExpect(this.b6.format(), "16oz JPLicks mint-chip (with sprinkles)")
+        && t.checkExpect(this.b7.format(), "16oz Rooibos (without mixins)")
+        && t.checkExpect(this.b8.format(),
+            "20oz HaagenDazs chocolate (with whipped cream, cherry ,sprinkles)");
   }
 }
