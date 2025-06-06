add33f39de8118d76f09b4f035222341650bb306
Vignesh Saravanakumar <vignesh.saravanakumar.vs@gmail.com>
2025-06-06 18:26:36 -0400

    Homework 8 Complete Deque

--- Homeworks/Homework 8/Deque.java
+++ Homeworks/Homework 8/Deque.java
@@ -1,3 +1,4 @@
+import tester.Tester;
 import java.util.function.Predicate;
 
 // represents an abstract node in the deque
@@ -151,32 +152,156 @@ class ExamplesDeque {
   Node<String> nodeE = new Node<String>("kiwi", this.deque3.header, this.nodeD);
 
   // test size
-  boolean testSize() {
-    return true;
+  boolean testSize(Tester t) {
+    Deque<String> test2 = new Deque<String>();
+    test2.addAtHead("single");
+
+    Deque<String> test3 = new Deque<String>();
+    test3.addAtTail("first");
+    test3.addAtTail("second");
+    test3.addAtTail("third");
+
+    Deque<Integer> test4 = new Deque<Integer>();
+    test4.addAtHead(1);
+    test4.addAtTail(2);
+
+    return t.checkExpect(new Deque<String>().size(), 0) &&
+        t.checkExpect(test2.size(), 1) &&
+        t.checkExpect(test3.size(), 3) &&
+        t.checkExpect(test4.size(), 2);
   }
 
   // test addAtHead
-  boolean testAddAtHead() {
-    return true;
+  boolean testAddAtHead(Tester t) {
+    Deque<String> test1 = new Deque<String>();
+    test1.addAtHead("solo");
+    String head1 = test1.removeFromHead();
+
+    Deque<String> test2 = new Deque<String>();
+    test2.addAtHead("first");
+    test2.addAtHead("second");
+    test2.addAtHead("third");
+    String head2 = test2.removeFromHead();
+
+    Deque<String> test3 = new Deque<String>();
+    test3.addAtTail("tail");
+    test3.addAtHead("head");
+    String tail3 = test3.removeFromTail();
+
+    Deque<String> test4 = new Deque<String>();
+    test4.addAtHead("a");
+    test4.addAtHead("b");
+    test4.addAtHead("c");
+    String tail4 = test4.removeFromTail();
+
+    return t.checkExpect(head1, "solo") &&
+        t.checkExpect(head2, "third") &&
+        t.checkExpect(tail3, "tail") &&
+        t.checkExpect(tail4, "a");
   }
 
   // test addAtTail
-  boolean testAddAtTail() {
-    return true;
+  boolean testAddAtTail(Tester t) {
+    Deque<String> test1 = new Deque<String>();
+    test1.addAtTail("only");
+    String removed1 = test1.removeFromTail();
+
+    Deque<String> test2 = new Deque<String>();
+    test2.addAtTail("x");
+    test2.addAtTail("y");
+    test2.addAtTail("z");
+    String tail2 = test2.removeFromTail();
+
+    Deque<String> test3 = new Deque<String>();
+    test3.addAtHead("front");
+    test3.addAtTail("back");
+    String head3 = test3.removeFromHead();
+
+    Deque<String> test4 = new Deque<String>();
+    test4.addAtTail("1");
+    test4.addAtTail("2");
+    test4.addAtTail("3");
+    String head4 = test4.removeFromHead();
+
+    return t.checkExpect(removed1, "only") &&
+        t.checkExpect(tail2, "z") &&
+        t.checkExpect(head3, "front") &&
+        t.checkExpect(head4, "1");
   }
 
   // test removeFromHead
-  boolean testRemoveFromHead() {
-    return true;
+  boolean testRemoveFromHead(Tester t) {
+    Deque<String> test1 = new Deque<String>();
+    test1.addAtHead("single");
+    String removed1 = test1.removeFromHead();
+    int size1 = test1.size();
+
+    Deque<String> test2 = new Deque<String>();
+    test2.addAtTail("a");
+    test2.addAtTail("b");
+    test2.addAtTail("c");
+    String first = test2.removeFromHead();
+    String second = test2.removeFromHead();
+
+    Deque<Integer> test3 = new Deque<Integer>();
+    test3.addAtHead(1);
+    test3.addAtTail(2);
+    test3.addAtHead(3);
+    Integer removed3 = test3.removeFromHead();
+
+    Deque<String> test4 = new Deque<String>();
+
+    return t.checkExpect(removed1, "single") &&
+        t.checkExpect(size1, 0) &&
+        t.checkExpect(first, "a") &&
+        t.checkExpect(second, "b") &&
+        t.checkExpect(removed3, 3) &&
+        t.checkException(new RuntimeException("Cannot remove from an empty list"),
+            test4, "removeFromHead");
   }
 
   // test removeFromTail
-  boolean testRemoveFromTail() {
-    return true;
+  boolean testRemoveFromTail(Tester t) {
+    Deque<String> test1 = new Deque<String>();
+    test1.addAtTail("alone");
+    String removed1 = test1.removeFromTail();
+    int size1 = test1.size();
+
+    Deque<String> test2 = new Deque<String>();
+    test2.addAtHead("x");
+    test2.addAtHead("y");
+    test2.addAtHead("z");
+    String last = test2.removeFromTail();
+    String secondLast = test2.removeFromTail();
+
+    Deque<Integer> test3 = new Deque<Integer>();
+    test3.addAtTail(10);
+    test3.addAtHead(20);
+    test3.addAtTail(30);
+    Integer removed3 = test3.removeFromTail();
+
+    // Test 4: Exception on empty deque
+    Deque<String> test4 = new Deque<String>();
+
+    return t.checkExpect(removed1, "alone") &&
+        t.checkExpect(size1, 0) &&
+        t.checkExpect(last, "x") &&
+        t.checkExpect(secondLast, "y") &&
+        t.checkExpect(removed3, 30) &&
+        t.checkException(new RuntimeException("Cannot remove from an empty list"),
+            test4, "removeFromTail");
   }
 
   // test find
-  boolean testFind() {
-    return true;
+  boolean testFind(Tester t) {
+    ANode<String> found1 = this.deque2.find(s -> s.equals("cde"));
+    ANode<String> notFound = this.deque2.find(s -> s.equals("xyz"));
+    ANode<String> emptyFind = this.deque1.find(s -> true);
+    ANode<String> firstMatch = this.deque3.find(s -> s.contains("a"));
+
+    return t.checkExpect(found1, this.node3) &&
+        t.checkExpect(notFound, this.deque2.header) &&
+        t.checkExpect(emptyFind, this.deque1.header) &&
+        t.checkExpect(firstMatch, this.nodeA);
   }
 }
